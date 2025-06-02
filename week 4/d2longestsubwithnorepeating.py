b66f3c138abac766b2f8fe856381a6a606126975
biplovgautam <madhavbiplov@gmail.com>
2025-06-02 17:25:45 +0545

    Day 23 - Find the length of the longest substring without repeating characters

---
+++ week 4/d2longestsubwithnorepeating.py
@@ -0,0 +1,51 @@
+"""3. Longest Substring Without Repeating Characters
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces."""
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_index = {}  # stores the last index of each character
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] + 1  # move left pointer
+
+            char_index[s[right]] = right  # update latest index
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
