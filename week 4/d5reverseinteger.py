c476e258f5b7712cee63e7db0d1757966d57a657
biplovgautam <madhavbiplov@gmail.com>
2025-06-05 16:14:45 +0545

    Day 26 - Reverse a signed 32-bit integer with overflow handling

---
+++ week 4/d5reverseinteger.py
@@ -0,0 +1,45 @@
+"""7. Reverse Integer
+Medium
+Topics
+premium lock icon
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1"""
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x_abs = abs(x)
+        reversed_str = str(x_abs)[::-1]
+        reversed_int = int(reversed_str) * sign
+
+        if reversed_int < INT_MIN or reversed_int > INT_MAX:
+            return 0
+        return reversed_int
